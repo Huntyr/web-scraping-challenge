3451a5aef79a676be435c3ff5e2f7ce34199ec35
Drew Alternate <39842385+Huntyr@users.noreply.github.com>
2019-09-03 23:46:51 -0700

    first commit

---
+++ Mission_To_Mars/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
